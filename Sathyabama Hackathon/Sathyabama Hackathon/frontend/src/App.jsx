42c1c910375f8069d01183a5bc9d450e5e09af49
Harishviswa1 <harishviswanath017@gmail.com>
2024-09-12 11:30:25 +0530

    Commit-4

--- Sathyabama Hackathon/Sathyabama Hackathon/frontend/src/App.jsx
+++ Sathyabama Hackathon/Sathyabama Hackathon/frontend/src/App.jsx
@@ -3,15 +3,11 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login/Login';
 import Doctorside from './components/Doctor-side';
 import Details from './components/Details';
+import User from './components/User/User';
+
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} /> 
-        <Route path="/doctor" element={<Doctorside />} /> 
-        <Route path="/details" element={<Details />} /> 
-      </Routes>
-    </Router>
+    <User/>
   );
 }
 
