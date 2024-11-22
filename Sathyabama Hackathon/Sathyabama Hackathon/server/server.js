dba24aa63838eb68eb22bd8b834bb9adb3161ae2
Giriraj S A <giriraj.sa2022it@sece.ac.in>
2024-11-22 11:42:29 +0530

    Doctor side UI changes

--- Sathyabama Hackathon/Sathyabama Hackathon/server/server.js
+++ Sathyabama Hackathon/Sathyabama Hackathon/server/server.js
@@ -15,7 +15,9 @@ const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
+   
  
+
 app.use(cors({
     origin: 'http://localhost:5173', 
     methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
@@ -25,7 +27,8 @@ app.use('/api/user/uploads', express.static(path.join(__dirname, 'uploads')));
 app.get('/api/user/uploads/:filename', (req, res) => {
   const filePath = path.join(__dirname, 'uploads', req.params.filename);
   res.sendFile(filePath, (err) => {
-      if (err) {
+    if (err) {
+        
           console.error(err);
           res.status(err.status).end();
       }
@@ -40,5 +43,5 @@ setInterval(checkAndUpdateAppointments, 24 * 60 * 60 * 1000);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`  );
 });
